e33a815b38f41a4db074276032ecb2255916452c
Roberto Aguayo <roberto.aguayo@endurance.com>
2018-08-22 14:57:27 -0400

    Added format decoration.

--- perl.info.pl
+++ perl.info.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/perl
 
-print map { $_ = "$_ : $ENV{$_}\n" } keys(%ENV);
+print "##########\n";
+print "ENV\t:\tVAL\n";
+print map { $_ = "$_\t:\t$ENV{$_}\n" } keys(%ENV);
 
-print map { ( -d $_ ) . ': ' . $_ . "\n" } @INC;
+print "##########\n";
+print "is_dir\t:\t\@INC\n";
+print map { ( -d $_ ? 1 : 0 ) . "\t:\t" . $_ . "\n" } @INC;
 
 exit 0;
 
